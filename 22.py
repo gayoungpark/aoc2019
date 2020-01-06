ea7257e5f245dfe8b08b5ae6522943eabfb129b6
GaYoung Park <gayoung@tubularlabs.com>
2020-01-05 17:42:18 -0800

    Add problem 22 part 2

--- 22.py
+++ 22.py
@@ -6,6 +6,7 @@ def main():
         shuffles = [line.rstrip('\n') for line in file.readlines()]
 
     print(f'part1: {part1(shuffles)}')
+    print(f'part2: {part2(shuffles)}')
 
 def part1(shuffles):
     size = 10007
@@ -33,6 +34,50 @@ def part1(shuffles):
         if deck[i] == 2019:
             return i
     return -1
+
+def part2(shuffles):
+    size = 119315717514047
+
+    start, jump = 0, 1
+    for shuffle in shuffles:
+        technique = shuffle.split(' ')
+        if len(technique) == 2:
+            # cut n
+            n = int(technique[-1])
+            start = (start + jump * n) % size
+        elif technique[2] == 'increment':
+            # deal with increment n
+            n = int(technique[-1])
+            mod_inv = modinv(n, size)
+            jump = (jump * mod_inv) % size
+        else:
+            # deal into new stack
+            jump = (jump * (size - 1)) % size
+            start = (start + jump) % size
+    start, jump = repeat(start, jump, 101741582076661, size)
+
+    return (start + 2020 * jump) % size
+
+def repeat(s, m, k, p):
+    m_k = pow(m, k, p)
+    s_k = (s * ((1 - m_k) % p) * modinv((1 - m) % p, p)) % p
+    return s_k, m_k
+
+
+# https://stackoverflow.com/questions/4798654/modular-multiplicative-inverse-function-in-python
+def egcd(a, b):
+    if a == 0:
+        return (b, 0, 1)
+    else:
+        g, y, x = egcd(b % a, a)
+        return (g, x - (b // a) * y, y)
+
+def modinv(a, m):
+    g, x, y = egcd(a, m)
+    if g != 1:
+        raise Exception('modular inverse does not exist')
+    else:
+        return x % m
 
 
 if __name__ == '__main__':
