2c3e3af94de95b0d0cafacb54af224ee56da3f36
GaYoung Park <gayoung@tubularlabs.com>
2020-01-20 17:25:15 -0800

    Add problem 18 part 1

--- 18.py
+++ 18.py
@@ -1,15 +1,22 @@
 #!/usr/bin/env python
 
 
+from collections import namedtuple
 from copy import deepcopy
 from enum import Enum
+import heapq
 
 
 def main():
     with open('18.txt', 'r') as file:
         raw_map = [line.rstrip('\n') for line in file.readlines()]
-    m = parse_map(raw_map)
-    print(m)
+
+    print(f'part1: {part1(raw_map)}')
+
+
+def part1(raw_map):
+    m, state = parse_map(raw_map)
+    return m.find_all_keys(state)
 
 
 class TileType(Enum):
@@ -20,8 +27,8 @@ class TileType(Enum):
 
 
 class Tile:
-    def __init__(self, pos, tile_type, char):
-        self.pos = pos
+    def __init__(self, point, tile_type, char):
+        self.point = point
         self.type = tile_type
         self.char = char
 
@@ -29,23 +36,94 @@ class Tile:
         return f'{self.char}'
 
 
+Point = namedtuple('Point', ['x', 'y'])
+
+
+class State:
+    def __init__(self, point, keys=None):
+        self.keys = keys or frozenset()
+        self.point = point
+
+    def __repr__(self):
+        return f'{self.point}, {self.keys}'
+
+
 class Map:
-    def __init__(self, m, me, keys, doors):
+    def __init__(self, m, keys, doors):
         self.m = m
-        self.me = me
         self.keys = keys
         self.doors = doors
 
     def __repr__(self):
-        m_copy = deepcopy(self.m)
-        m_copy[self.me[1]][self.me[0]] = '@'
         m = ''
-        for y in range(len(m_copy)):
+        for y in range(len(self.m)):
             row = ''
-            for x in range(len(m_copy[0])):
-                row += f'{m_copy[y][x]}'
+            for x in range(len(self.m[0])):
+                row += f'{self.m[y][x]}'
             m += row + '\n'
         return m
+
+    def find_keys(self, state):
+        queue = []
+        queue.append((state.point, 0))
+        visited = set()
+
+        keys = []
+        while len(queue):
+            point, dist = queue.pop(0)
+            # check if point is at a key
+            tile = self.m[point.y][point.x]
+            if tile.type == TileType.KEY and tile.char not in state.keys:
+                keys.append((point, dist))
+            else:
+                # add all unvisited neighbors
+                neighbors = self.get_neighbors(point, state.keys)
+                for neighbor in neighbors:
+                    if neighbor not in visited:
+                        queue.append((neighbor, dist + 1))
+            visited.add(point)
+        return keys
+
+    def get_neighbors(self, point, keys):
+        possibilities = [
+            (point.x + 1, point.y),
+            (point.x - 1, point.y),
+            (point.x, point.y + 1),
+            (point.x, point.y - 1),
+        ]
+
+        neighbors = []
+        for nx, ny in possibilities:
+            tile = self.m[ny][nx]
+            if tile.type == TileType.DOOR:
+                if tile.char.lower() in keys:
+                    neighbors.append(Point(nx, ny))
+            elif tile.type != TileType.WALL:
+                neighbors.append(Point(nx, ny))
+        return neighbors
+
+    def find_all_keys(self, initial_state):
+        pq = [(0, 0, initial_state)]
+        visited = set()
+        i = 1   # insert order to break ties in heap
+
+        while True:
+            dist, _, state = heapq.heappop(pq)
+            if (state.point, state.keys) in visited:
+                continue
+            # check if we found all the keys
+            if len(state.keys) == len(self.keys):
+                return dist
+            # add all unvisited neighboring keys
+            keys = self.find_keys(state)
+            for npoint, ndist in keys:
+                new_keys = state.keys.union(set(self.m[npoint.y][npoint.x].char))
+                if (npoint, new_keys) not in visited:
+                    new_dist = dist + ndist
+                    heapq.heappush(pq, (new_dist, i, State(npoint, new_keys)))
+                    i += 1
+            visited.add((state.point, state.keys))
+        return -1
 
 
 def parse_map(raw_m):
@@ -56,24 +134,24 @@ def parse_map(raw_m):
     for y in range(len(raw_m)):
         row = []
         for x in range(len(raw_m[0])):
-            pos = (x, y)
+            point = Point(x, y)
             elem = raw_m[y][x]
             if elem == '#':
-                tile = Tile(pos, TileType.WALL, elem)
+                tile = Tile(point, TileType.WALL, elem)
             elif elem == '.':
-                tile = Tile(pos, TileType.PATH, elem)
+                tile = Tile(point, TileType.PATH, elem)
             elif elem == '@':
-                me = pos
-                tile = Tile(pos, TileType.PATH, '.')
+                me = point
+                tile = Tile(point, TileType.PATH, '.')
             elif ord('a') <= ord(elem) <= ord('z'):
-                tile = Tile(pos, TileType.KEY, elem)
+                tile = Tile(point, TileType.KEY, elem)
                 keys.append(tile)
             elif ord('A') <= ord(elem) <= ord('Z'):
-                tile = Tile(pos, TileType.DOOR, elem)
+                tile = Tile(point, TileType.DOOR, elem)
                 doors.append(tile)
             row.append(tile)
         m.append(row)
-    return Map(m, me, keys, doors)
+    return Map(m, keys, doors), State(me)
 
 
 if __name__ == '__main__':
